e6c658352398786470b207268e2772b8b517f637
Pete A <peter.ayresmusic@gmail.com>
2019-01-25 00:03:13 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,9 +0,0 @@
-var greeting = "Hello, everybody!";
-
-var specialGuest =  "Neil deGrasse Tyson"
-
-var greetSpecialGuest = `Hello, ${specialGuest}!`
-
-var topic = 'sex';
-
-var conversation = `Let's talk about ${topic}.`;
